26980b94c35d89391e2811b429b1d39552ab0bf6
Alex Mullans <mullanaj@rose-hulman.edu>
2011-03-24 15:09:13 -0400

    Adding Java project and basic test.

---
+++ src/PrimeNumberTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrimeNumberTest {
+	@Test
+	public void testImports() {
+		assertTrue("JUnit doesn't like you", true);
+	}
+}
